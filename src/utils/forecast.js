85f8c1b81a4c199d63c417087d204befe8a4bd40
DwanW <kunvang@yahoo.com>
2020-09-06 14:10:58 -0600

    updated forecast description

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (lat, long, callback) => {
         } else if (response.error){
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, `${current.weather_descriptions[0]}.it is currently ${current.temperature} degrees, it feels like ${current.feelslike}.`)
+            callback(undefined, `${current.weather_descriptions[0]}.it is currently ${current.temperature} degrees, it feels like ${current.feelslike}. The chance of precipitation is ${current.precip * 100}%`)
         }
     })
 } 
